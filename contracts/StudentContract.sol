2b2b9b5155a869b9e7e084af32c46633166c7abc
João Vitor Leite Machado <joao.machado@ekinops.com>
2023-01-17 13:32:20 -0300

    adicionando contrato para consultar e adicionar estudantes

--- contracts/StudentContract.sol
+++ contracts/StudentContract.sol
@@ -8,4 +8,39 @@ contract StudentsContract {
         uint studentAge;
     }
 
+    mapping(address => Student) private _enrolledStudents;
+
+    function getEnrolledStudentByAddress(address _studentAdress) public view returns (Student memory){
+
+        return _enrolledStudents[_studentAdress];
+
+    }
+
+    function isStudentAgeValid(uint _studentAge) private pure returns(bool) {
+        return _studentAge > 0;
+    }
+
+    function isStudentNameValid(string memory _studentName) private pure returns (bool) {
+        bytes memory tempString = bytes(_studentName);
+        return tempString.length>0;
+    }
+
+    function isStudentValid(Student memory _student) private pure returns (bool) {
+        return isStudentAgeValid(_student.studentAge) && isStudentNameValid(_student.studentName);
+    }
+
+    function enrollStudent(string memory _studentName, uint _studentAge) public {
+        require(!isStudentValid(
+            getEnrolledStudentByAddress(msg.sender)
+        ), 
+        "Student already enrolled"
+        );
+
+        Student memory student = Student(_studentName, _studentAge);
+
+        assert(isStudentValid(student));
+
+        _enrolledStudents[msg.sender] = student;
+    }
+
 }
