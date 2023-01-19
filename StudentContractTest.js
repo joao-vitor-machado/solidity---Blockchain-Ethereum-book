5b07b6f64d50c9561758ee7ef72b84ee6df04fe2
João Vitor Leite Machado <joao.machado@ekinops.com>
2023-01-19 08:47:03 -0300

    utilizando teste especifico de teste para o contrato de estudantes

---
+++ StudentContractTest.js
@@ -0,0 +1,40 @@
+const assert = require('assert');
+const Web3 = require('web3');
+const {abiJson, bytecode} = require('./scripts/compile.js');
+
+const web3 = new Web3('HTTP://127.0.0.1:7545');
+
+let accounts;
+let account_1;
+let account_2;
+let studentsContract;
+
+beforeEach(async() => {
+    accounts = await web3.eth.getAccounts();
+    account_1 = accounts[0];
+    account_2 = accounts[1];
+
+    studentsContract = 
+        await new web3.eth.Contract(abiJson)
+            .deploy({data: "0x"+bytecode})
+            .send({from: account_1, gas: 1000000});
+});
+
+describe('Students Contract', async() => {
+    it('Should return valid student', async() => {
+        
+        let name = 'João vitor';
+        let age = 20;
+
+        await studentsContract.methods
+            .enrollStudent(name, age)
+            .send({from: account_1, gas: 1000000})
+
+        let student = await studentsContract.methods
+            .getEnrolledStudentByAddress(account_1)
+            .call();
+
+        console.log(student)
+
+    })
+})
